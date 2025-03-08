b8cb169326a8ed152770ce6252b8e48849abafa7
jesus_plata <jesusplata1520@gmail.com>
2025-03-08 16:28:43 -0500

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles.css'
+import {HeroesApp} from './HeroesApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HeroesApp />
   </StrictMode>,
 )
