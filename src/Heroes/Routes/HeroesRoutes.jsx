b8e85a734672854ebfd9b5883fcc70e2d7d543f7
jesus_plata <jesusplata1520@gmail.com>
2025-03-11 10:55:57 -0500

    a

---
+++ src/Heroes/Routes/HeroesRoutes.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navbar } from '../../ui/components/navbar'
+import { Route,Routes,Navigate } from 'react-router-dom'
+import {MarvelPage} from '../Pages/MarvelPage'
+import {DcPages} from '../Pages/DcPages'
+import { Search } from '../Pages/Search'
+export const HeroesRoutes = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className='container'>
+
+   
+
+    <Routes>
+        <Route path="/marvel" element={<MarvelPage />} />
+        <Route path="/dc" element={<DcPages />} />
+        <Route  path="/search" element={< Search/>}/>
+       <Route />
+           
+        <Route path='/' element={<Navigate to="/marvel"/>}/>
+      </Routes>
+      </div>
+    </>
+  )
+}
--- src/ui/components/navbar.jsx
+++ src/ui/components/navbar.jsx
@@ -1,46 +1,65 @@
-import { Link, NavLink } from 'react-router-dom';
-
+import { Link, NavLink,useNavigate } from 'react-router-dom';
 
 export let Navbar = () => {
+    
+
+let navigate = useNavigate();
+
+    let onLogout=()=>{
+navigate('/login',{
+replace:true
+})
+
+    }
+
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
-            <Link 
-                className="navbar-brand" 
-                 to="/"
-            >
+            <Link className="navbar-brand" to="/">
                 Asociaciones
             </Link>
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
-
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        className="nav-item nav-link" 
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/dc"
                     >
                         DC
                     </NavLink>
+
+                    <NavLink 
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        to="/Search"
+                    >
+                        Search
+                    </NavLink>
+
+                    <NavLink 
+                    to='/login'
+                    >
+
+                    </NavLink>
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/login"
-                    >
-                        Login
-                    </NavLink>
+                    <span className="nav-item nav-link text-info">
+                        Jesus Plata
+                    </span>
+                    <button className="nav-item nav-link btn btn-outline"
+                    onClick={onLogout}
+                    >Logout</button>
                 </ul>
             </div>
         </nav>
-    )
+    );
 }
